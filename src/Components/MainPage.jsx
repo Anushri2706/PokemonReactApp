15bf14cd4f221ddf9d11d3d190efc5d051274fef
Anushri <anushri_gupta@outlook.com>
2023-11-11 23:49:14 +0100

    everything works;)

--- src/Components/MainPage.jsx
+++ src/Components/MainPage.jsx
@@ -7,7 +7,6 @@ function MainPage (){
       <h1>hi</h1>
       <h2>Welcome to my Pokemon app</h2>
       <h3>Go to Pokemon List and click on an image to get started</h3>
-
     </div>
     )
 }
--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -6,7 +6,7 @@ function MainPage() {
     <div>
       <>
       <Main/>
-      </>
+\      </>
     </div>
   )
 }
