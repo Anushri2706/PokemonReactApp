a96d9a069a348c25708140bb1f320100afc57300
Anushri2706 <anushri.gupta@maersk.com>
2025-10-06 14:17:54 +0200

    fix

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -2,11 +2,12 @@ import React from 'react';
 import Main from '../Components/Main';
 
 function MainPage() {
+  // Test comment to trigger workflow validation
   return (
     <div>
       <>
       <Main/>
-      <div>Omg</div>
+      <div>Omg - Testing workflow</div>
       </>
     </div>
   )
